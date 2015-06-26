5133e4f92a33c4657176b3571c7a2bba25644bd3
fitnazo <fitnazo.com@gmail.com>
2015-06-26 22:28:24 +0530

    Spacing updated as per code

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,8 +4,8 @@ require "calc"
 RSpec.describe Calc do
   describe "# sum" do
     it "returns sum of 2 numbers" do
-      calc   =  Calc.new # setup
-      result =  calc.sum(2, 3) # exercise
+      calc = Calc.new # setup
+      result = calc.sum(2, 3) # exercise
       expect(result).to eq(5) # verify
     end
   end
